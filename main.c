947bd6c27edbd93f1c6b2185709e398a090247aa
simylein <67859356+simylein@users.noreply.github.com>
2025-04-28 22:30:36 +0200

    wip(sleep): trying for deep sleep functionality

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include "hardware/adc.h"
+#include "hardware/timer.h"
+#include "pico/sleep.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
 const uint LED_PIN = 25;
 const uint VSYS_PIN = 29;
 
-int main() {
+void wake(uint alarm) { printf("waking up from sleep\n"); }
+
+int main(void) {
   stdio_init_all();
+  sleep_ms(2000);
 
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -14,11 +19,21 @@ int main() {
   adc_init();
   adc_gpio_init(VSYS_PIN);
 
+  gpio_put(LED_PIN, 1);
+  sleep_ms(50);
+  gpio_put(LED_PIN, 0);
+
   while (true) {
+    sleep_ms(1850);
+    // sleep_goto_sleep_for(1850, &wake);
+
     gpio_put(LED_PIN, 1);
     sleep_ms(50);
     gpio_put(LED_PIN, 0);
-    sleep_ms(950);
+    sleep_ms(50);
+    gpio_put(LED_PIN, 1);
+    sleep_ms(50);
+    gpio_put(LED_PIN, 0);
 
     uint16_t raw_value = adc_read();
     float voltage = (raw_value / 4095.0) * 3.3;
