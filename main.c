acdebba141025da0c13fcae1aa62bfbde02aeb52
simylein <67859356+simylein@users.noreply.github.com>
2025-04-28 19:14:01 +0200

    feat(main): measure vsys voltage

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
+#include "hardware/adc.h"
 #include "pico/stdlib.h"
+#include <stdio.h>
+
+const uint LED_PIN = 25;
+const uint VSYS_PIN = 29;
 
 int main() {
-  const uint LED_PIN = 25;
+  stdio_init_all();
 
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
+  adc_init();
+  adc_gpio_init(VSYS_PIN);
+
   while (true) {
     gpio_put(LED_PIN, 1);
-    sleep_ms(100);
+    sleep_ms(50);
     gpio_put(LED_PIN, 0);
-    sleep_ms(900);
+    sleep_ms(950);
+
+    uint16_t raw_value = adc_read();
+    float voltage = (raw_value / 4095.0) * 3.3;
+    printf("vsys %.3fv\n", voltage);
   }
 }
