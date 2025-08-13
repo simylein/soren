b83d3b44aa3d6be1435e9c6b06b13d4d6e0236cb
simylein <67859356+simylein@users.noreply.github.com>
2025-08-13 21:00:20 +0200

    chore(main): working restore point

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include "ads1115.h"
 #include "pcf8563.h"
 #include "rp2040.h"
 #include "si7021.h"
@@ -9,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-const uint16_t id = 0xa391;
+const uint16_t id = 0x0000;
 
 uint16_t hton16(uint16_t value) { return (value << 8) | (value >> 8); }
 
@@ -18,31 +17,29 @@ int main(void) {
 	rp2040_led_init();
 	pcf8563_init();
 	si7021_init();
-	ads1115_init();
 	sx1278_init();
 	sx1278_sleep();
 
 	while (true) {
-		rp2040_led_blink(1, 50, true);
+		// rp2040_led_blink(1, 50, true);
 
 		rtc_t time = pcf8563_time();
 		uint16_t temperature;
 		si7021_temperature(&temperature);
 		uint16_t humidity;
 		si7021_humidity(&humidity);
-		printf("time %02d:%02d:%02d temperature %.2f humidity %.2f\n", time.hour, time.minute, time.second,
-					 si7021_temperature_human(temperature), si7021_humidity_human(humidity));
-		int16_t photovoltaic;
-		ads1115_photovoltaic(&photovoltaic);
-		int16_t battery;
-		ads1115_battery(&battery);
-		printf("time %02d:%02d:%02d photovoltaic %.3f battery %.3f\n", time.hour, time.minute, time.second,
-					 ads1115_photovoltaic_human(photovoltaic), ads1115_battery_human(battery));
+		printf("temperature %.2f humidity %.2f\n", si7021_temperature_human(temperature), si7021_humidity_human(humidity));
+		// int16_t photovoltaic;
+		// ads1115_photovoltaic(&photovoltaic);
+		// int16_t battery;
+		// ads1115_battery(&battery);
+		// printf("time %02d:%02d:%02d photovoltaic %.3f battery %.3f\n", time.hour, time.minute, time.second,
+		// 			 ads1115_photovoltaic_human(photovoltaic), ads1115_battery_human(battery));
 
-		rp2040_led_blink(2, 50, true);
+		// rp2040_led_blink(2, 50, true);
 
 		sx1278_standby();
-		sx1278_frequency(433 * 1000 * 1000);
+		sx1278_frequency(434225 * 1000);
 		sx1278_tx_power(2);
 		sx1278_coding_rate(5);
 		sx1278_bandwidth(125 * 1000);
@@ -62,29 +59,29 @@ int main(void) {
 		tx_data_len += sizeof(humidity);
 		sx1278_send(&tx_data, tx_data_len, 1000);
 
-		rp2040_led_blink(3, 50, true);
+		// rp2040_led_blink(3, 50, true);
 
-		// uint8_t rx_data[256];
-		// uint8_t rx_data_len = 0;
-		// sx1278_recv(&rx_data, &rx_data_len, 1000);
+		uint8_t rx_data[256];
+		uint8_t rx_data_len = 0;
+		sx1278_recv(&rx_data, &rx_data_len, 1000);
 
-		// if (rx_data_len != 0) {
-		// 	rp2040_led_blink(4, 50, true);
-		// }
+		if (rx_data_len != 0) {
+			// rp2040_led_blink(4, 50, true);
+		}
 
-		tx_data_len = 0;
-		memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(id)}, sizeof(id));
-		tx_data_len += sizeof(id);
-		kind = 0x02;
-		memcpy(&tx_data[tx_data_len], &kind, sizeof(kind));
-		tx_data_len += sizeof(kind);
-		memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(photovoltaic)}, sizeof(photovoltaic));
-		tx_data_len += sizeof(photovoltaic);
-		memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(battery)}, sizeof(battery));
-		tx_data_len += sizeof(battery);
-		sx1278_send(&tx_data, tx_data_len, 1000);
+		// tx_data_len = 0;
+		// memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(id)}, sizeof(id));
+		// tx_data_len += sizeof(id);
+		// kind = 0x02;
+		// memcpy(&tx_data[tx_data_len], &kind, sizeof(kind));
+		// tx_data_len += sizeof(kind);
+		// memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(photovoltaic)}, sizeof(photovoltaic));
+		// tx_data_len += sizeof(photovoltaic);
+		// memcpy(&tx_data[tx_data_len], &(uint16_t[]){hton16(battery)}, sizeof(battery));
+		// tx_data_len += sizeof(battery);
+		// sx1278_send(&tx_data, tx_data_len, 1000);
 
-		rp2040_led_blink(3, 50, true);
+		// rp2040_led_blink(3, 50, true);
 
 		// rx_data_len = 0;
 		// sx1278_recv(&rx_data, &rx_data_len, 1000);
@@ -96,7 +93,7 @@ int main(void) {
 		sx1278_standby();
 		sx1278_sleep();
 
-		// sleep_ms(10 * 1000);
+		// sleep_ms(60 * 1000);
 
 		pcf8563_alarm_schedule(1);
 		printf("entering dormant sleep\n");
@@ -107,7 +104,6 @@ int main(void) {
 		rp2040_led_init();
 		pcf8563_init();
 		si7021_init();
-		ads1115_init();
 		sx1278_init();
 
 		pcf8563_alarm_clear();
