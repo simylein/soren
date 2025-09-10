715f2b8031a53ad1ce7f268594f4c205c5ad3887
simylein <67859356+simylein@users.noreply.github.com>
2025-09-10 06:39:18 +0200

    feat(rp2040): adc sample five and take median

--- src/rp2040.c
+++ src/rp2040.c
@@ -3,6 +3,18 @@
 #include "logger.h"
 #include <pico/stdlib.h>
 
+void sort(uint16_t *items, uint8_t items_len) {
+	for (uint8_t index = 0; index < items_len - 1; index++) {
+		for (uint8_t ind = 0; ind < items_len - index - 1; ind++) {
+			if (items[ind] > items[ind + 1]) {
+				uint16_t store = items[ind];
+				items[ind] = items[ind + 1];
+				items[ind + 1] = store;
+			}
+		}
+	}
+}
+
 void rp2040_stdio_init(void) {
 	trace("rp2040 init stdio\n");
 
@@ -27,24 +39,34 @@ void rp2040_photovoltaic(uint16_t *photovoltaic) {
 	gpio_put(rp2040_en_photovoltaic, 1);
 
 	adc_select_input(rp2040_adc_photovoltaic);
-
 	adc_read();
-	*photovoltaic = adc_read();
+
+	uint16_t samples[5];
+	for (uint8_t index = 0; index < sizeof(samples) / sizeof(uint16_t); index++) {
+		samples[index] = adc_read();
+	}
+	sort(samples, sizeof(samples) / sizeof(uint16_t));
+	*photovoltaic = samples[sizeof(samples) / sizeof(uint16_t) / 2];
 
 	gpio_put(rp2040_en_photovoltaic, 0);
 }
 
-float rp2040_photovoltaic_human(uint16_t photovoltaic) { return (photovoltaic * 3.3) / 4095.0; }
+float rp2040_photovoltaic_human(uint16_t photovoltaic) { return (photovoltaic * 3.3f) / 4095.0f; }
 
 void rp2040_battery(uint16_t *battery) {
 	gpio_put(rp2040_en_battery, 1);
 
 	adc_select_input(rp2040_adc_battery);
-
 	adc_read();
-	*battery = adc_read();
+
+	uint16_t samples[5];
+	for (uint8_t index = 0; index < sizeof(samples) / sizeof(uint16_t); index++) {
+		samples[index] = adc_read();
+	}
+	sort(samples, sizeof(samples) / sizeof(uint16_t));
+	*battery = samples[sizeof(samples) / sizeof(uint16_t) / 2];
 
 	gpio_put(rp2040_en_battery, 0);
 }
 
-float rp2040_battery_human(uint16_t battery) { return (battery * 3.3) / 4095.0; }
+float rp2040_battery_human(uint16_t battery) { return (battery * 3.3f) / 4095.0f; }
